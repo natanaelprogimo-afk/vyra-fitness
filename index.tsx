962fe6b192dcd0d20eb70be4668fda5c19f1fa31
Natan <natan@example.com>
2026-03-04 19:06:32 -0300

    fix: remove 'Build Test' label from app entry

--- index.tsx
+++ index.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>🚀 Vyra Fitness - Build Test</Text>
+      <Text>🚀 Vyra Fitness</Text>
     </View>
   );
 }
